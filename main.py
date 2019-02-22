ac281558356ed4441592f23f4be350f37e16de0b
florent <florent@localhost.localdomain>
2019-02-22 13:42:39 +0100

    Initialisation du dépot

---
+++ main.py
@@ -0,0 +1,11 @@
+#! /usr/env python3
+# conding: utf-8
+
+import json
+from watson_developer_cloud import VisualRecognitionV3
+
+visual_recognition = VisualRecognitionV3(
+    version = '2018-03-19',
+    iam_apikey = 'sRbpnBe1LY_z9q0xNU4LVF1VxqYciJsv1avlLESc9yEy'
+)
+
